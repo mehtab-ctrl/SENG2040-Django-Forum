1b2702003b85315aa2e85aa8ba3ea4256575b0e9
mehtab-ctrl <126291818+mehtab-ctrl@users.noreply.github.com>
2025-04-10 06:02:54 -0400

    Loaded more posts with button click

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log('hello world')
 
 const helloWolrdBox = document.getElementById('hello-world')
-
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -12,5 +13,42 @@ $.ajax({
     },
     error: function(error){
         console.log('error',error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+        spinnerBox.classList.add('not-visible')
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">${el.title}</h5>
+                    <p class="card-text">${el.body}</p>
+                    </div>
+                    <div class="card-footer">
+                    <div class="row">
+                        <div class="col-1">
+                        <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                        <div class="col-1">
+                        <a href="#" class="btn btn-primary">Like</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            `
+             });
+        }, 100)
+    },
+
+    error: function(error){
+        console.log(error)
     }
 })
