5346beaabb49a5a6a3095bf1c3b9c68f4b3343eb
mehtab-ctrl <126291818+mehtab-ctrl@users.noreply.github.com>
2025-04-11 16:18:21 -0400

    added handle alert functions for success and error

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
+const postForm = document.getElementById('post-form')
+const title = document.getElementById('id_title')
+const body = document.getElementById('id_body')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const alertBox  = document.getElementById('alert-box')
+console.log('csrf', csrf[0].value)
+
 const getCookie =(name) => {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -31,13 +39,14 @@ const likeUnlikePosts = ()=> {
 
         $.ajax({
             type: 'POST',
-            url: "",
+            url: "like-unlike/",
             data: {
                 'csrfmiddlewaretoken': csrftoken,
                 'pk': clickedId,
             },
             success: function(response){
                 console.log(response)
+                clickedBtn.textContent = response.liked ? `Unlike (${response.count})`: `Like (${response.count})`
             },
             error: function(error){
                 console.log(error)
@@ -72,14 +81,15 @@ const getData = () => {
                             </div>
                             <div class="col-2">
                             <form class="like-unlike-forms" data-form-id="${el.id}">
-                                {% csrf_token %}
-                                <button href="#" class="btn btn-primary" id="like-unlike-${el.id}>${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                </form>
                             </div>
                         </div>
                     </div>
                     </div>
                 `
                  });
+                 likeUnlikePosts()
             }, 100)
             console.log(response.size)
             if (response.size === 0) {
@@ -101,6 +111,50 @@ loadBtn.addEventListener('click', ()=>{
     spinnerBox.classList.remove('not-visible')
     visible +=3
     getData()
+})
+
+postForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+            'title': title.value,
+            'body': body.value
+        },
+        success: function(response){
+            console.log(response)
+            postsBox.insertAdjacentHTML('afterbegin', `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${response.title}</h5>
+                        <p class="card-text">${response.body}</p>
+                        </div>
+                        <div class="card-footer">
+                        <div class="row">
+                            <div class="col-2">
+                            <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                            <form class="like-unlike-forms" data-form-id="${response.id}">
+                                <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    `)
+                    likeUnlikePosts()
+                    $('#addPostModal').modal('hide')
+                    handleAlerts('success', 'New post added!')
+        },
+        error: function(error){
+            console.log(error)
+            handleAlerts('danger','ups...Something went wrong')
+        }
+    })
 })
 
 getData()
